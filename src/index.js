f69811943ab8c6cc36a4d47a7bea511a8b885622
artem aleinik <a.aleinik@beltechsoft.com>
2020-03-12 23:33:11 +0300

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(string, bracketsConfig) {
+
+  if(string.length % 2 !== 0) return false;
+
+  for(let i = 0; i < 30; i++){
+    string = string.replace(new RegExp(bracketsConfig.map(value => value.map(value =>  /^\d+$/.test(value) ? value : '\\' + value   ).join('')).join('|'),"g"),'');
+  }
+ 
+  return !string
+
 }
